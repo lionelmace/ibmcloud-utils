a6b7bb52207d76428998bb9f16b0d67f3fc7500f
Lionel <lionel.mace@gmail.com>
2016-11-05 10:36:39 +0100

    added bmx.sh

---
+++ bmx.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Please choose a target:"
+select target in "Prod EU" "Prod US" "Prod AU" "Stage1 EU" "Stage1 US" "CIO DYS0 Dedicated" "Exit"; do
+  case $target in
+  "Prod US" )
+    export BMX_TARGET=ng
+    break;;
+  "Prod EU" )
+    export BMX_TARGET=eu-gb
+    break;;
+  "Prod AU" )
+    export BMX_TARGET=au-syd
+    break;;
+  "Stage1 US" )
+    export BMX_TARGET=stage1.ng
+    break;;
+  "Stage1 EU" )
+    export BMX_TARGET=stage1.eu-gb
+    break;;
+  "CIO DYS0 Dedicated" )
+    export BMX_TARGET=dys0
+    break;;
+  "Exit" )
+    exit;;
+  esac
+done
+
+export CF_API=https://api.$BMX_TARGET.bluemix.net
+export CF_HOME="$HOME/.cf-bmx/$BMX_TARGET"
+
+# if cf_home does not exist do a login
+if [ ! -d $CF_HOME ]; then
+  mkdir -p "$CF_HOME"
+  cf login -a $CF_API
+else
+  cf target
+fi
+
+rm -f $HOME/.cf
+ln -s $CF_HOME/.cf $HOME/.cf
+# ls -la $HOME/.cf
