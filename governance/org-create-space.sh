f9b38ba2b7e4953bd1d1ec56fa988474990d6af7
Lionel Mace <lionel.mace@gmail.com>
2018-07-04 15:16:40 +0200

    remove emails ibm

--- governance/org-create-space.sh
+++ governance/org-create-space.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 ##listofusers = firstname.lastname@fr.ibm.com xx.xxx@gmail.com xx.xxx@gmail.com
 
-
-# courtinb@fr.ibm.com, cmanghini@fr.ibm.com, wachs@fr.ibm.com, schimowski@fr.ibm.com, zoric@fr.ibm.com, decroos@fr.ibm.com, genard@fr.ibm.com, theuwiss@fr.ibm.com, mermet@fr.ibm.com, berthajm@fr.ibm.com, drascek@fr.ibm.com, peluttiero@fr.ibm.com, sauvanet@fr.ibm.com, gaultier@fr.ibm.com, rubiram@fr.ibm.com, casile@fr.ibm.com, o_vanbaelinghem@fr.ibm.com, favre@fr.ibm.com, ransanp@fr.ibm.com, droinpy@fr.ibm.com, vaniets@fr.ibm.com, outters@fr.ibm.com
-# fx.drouet@fr.ibm.com, gaetan.podda@fr.ibm.com, isabelle.drid@fr.ibm.com, sylvain.desnoes@fr.ibm.com, stephane.valette@fr.ibm.com, ludovic.hazard@fr.ibm.com, lionel.balmain@fr.ibm.com
-
 # Formation interne Kubernetes
-#for email in cmanghini@fr.ibm.com wachs@fr.ibm.com schimowski@fr.ibm.com zoric@fr.ibm.com decroos@fr.ibm.com genard@fr.ibm.com theuwiss@fr.ibm.com mermet@fr.ibm.com berthajm@fr.ibm.com drascek@fr.ibm.com peluttiero@fr.ibm.com sauvanet@fr.ibm.com gaultier@fr.ibm.com rubiram@fr.ibm.com casile@fr.ibm.com o_vanbaelinghem@fr.ibm.com favre@fr.ibm.com ransanp@fr.ibm.com droinpy@fr.ibm.com vaniets@fr.ibm.com outters@fr.ibm.com fx.drouet@fr.ibm.com gaetan.podda@fr.ibm.com isabelle.drid@fr.ibm.com sylvain.desnoes@fr.ibm.com stephane.valette@fr.ibm.com ludovic.hazard@fr.ibm.com lionel.balmain@fr.ibm.com
-# for email in franck_descollonges@fr.ibm.com lentini@fr.ibm.com courtinb@fr.ibm.com 
-#for email in slebrun@fr.ibm.com
-#for email in bidard@fr.ibm.com
-#for email in gpiedeloup@fr.ibm.com
-for email in courtinb@fr.ibm.com
+for email in firstname.lastname@fr.ibm.com xx.xxx@gmail.com xx.xxx@gmail.com
 
 do
 	## Extract last name from email
@@ -19,10 +10,10 @@ do
 
 	## Create a Cloud Foundry space with the last name
 	echo $lastname
-	#ibmcloud account space-create $lastname
+	ibmcloud account space-create $lastname
 
 	## Invite user and assign role developer to this space
-	#ibmcloud account user-invite $email -o cloud-europe -s $lastname --space-role SpaceDeveloper
+	ibmcloud account user-invite $email -o cloud-europe -s $lastname --space-role SpaceDeveloper
 
     ## Assign Policy
 	ibmcloud iam user-policy-create $email --roles Administrator --service-name containers-kubernetes
