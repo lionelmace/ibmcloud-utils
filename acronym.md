8cc1831473a1065a3f83c7eb8a045e709da93a0a
Lionel Mace <lionel.mace@gmail.com>
2018-07-04 14:43:04 +0200

    create user

--- acronym.md
+++ acronym.md
@@ -1,14 +1,15 @@
-# Acronym
+# Acronyms
 
-CAAS Container as a Service
-CAM  Cloud Automation Manager
-CF   Cloud Foundry
-IAM  Identity Access Manager
-IKS  IBM Cloud Kubernetes Servie
-K8S  Kubernetes
-POD  Point of Delivery
-POP  Point of Presence
-SRE  Site Reliability Engineer
-VA   Vulnerability Advisor
-VPC  Virtual Private Cloud
-WAF  Web Application Firewall
+- ALB  Application Load Balancer
+- CAAS Container as a Service
+- CAM  Cloud Automation Manager
+- CF   Cloud Foundry
+- IAM  Identity Access Manager
+- IKS  IBM Cloud Kubernetes Servie
+- K8S  Kubernetes
+- POD  Point of Delivery
+- POP  Point of Presence
+- SRE  Site Reliability Engineer
+- VA   Vulnerability Advisor
+- VPC  Virtual Private Cloud
+- WAF  Web Application Firewall
--- governance/org-create-space.sh
+++ governance/org-create-space.sh
@@ -6,8 +6,12 @@
 # fx.drouet@fr.ibm.com, gaetan.podda@fr.ibm.com, isabelle.drid@fr.ibm.com, sylvain.desnoes@fr.ibm.com, stephane.valette@fr.ibm.com, ludovic.hazard@fr.ibm.com, lionel.balmain@fr.ibm.com
 
 # Formation interne Kubernetes
-for email in cmanghini@fr.ibm.com wachs@fr.ibm.com schimowski@fr.ibm.com zoric@fr.ibm.com decroos@fr.ibm.com genard@fr.ibm.com theuwiss@fr.ibm.com mermet@fr.ibm.com berthajm@fr.ibm.com drascek@fr.ibm.com peluttiero@fr.ibm.com sauvanet@fr.ibm.com gaultier@fr.ibm.com rubiram@fr.ibm.com casile@fr.ibm.com o_vanbaelinghem@fr.ibm.com favre@fr.ibm.com ransanp@fr.ibm.com droinpy@fr.ibm.com vaniets@fr.ibm.com outters@fr.ibm.com fx.drouet@fr.ibm.com gaetan.podda@fr.ibm.com isabelle.drid@fr.ibm.com sylvain.desnoes@fr.ibm.com stephane.valette@fr.ibm.com ludovic.hazard@fr.ibm.com lionel.balmain@fr.ibm.com
+#for email in cmanghini@fr.ibm.com wachs@fr.ibm.com schimowski@fr.ibm.com zoric@fr.ibm.com decroos@fr.ibm.com genard@fr.ibm.com theuwiss@fr.ibm.com mermet@fr.ibm.com berthajm@fr.ibm.com drascek@fr.ibm.com peluttiero@fr.ibm.com sauvanet@fr.ibm.com gaultier@fr.ibm.com rubiram@fr.ibm.com casile@fr.ibm.com o_vanbaelinghem@fr.ibm.com favre@fr.ibm.com ransanp@fr.ibm.com droinpy@fr.ibm.com vaniets@fr.ibm.com outters@fr.ibm.com fx.drouet@fr.ibm.com gaetan.podda@fr.ibm.com isabelle.drid@fr.ibm.com sylvain.desnoes@fr.ibm.com stephane.valette@fr.ibm.com ludovic.hazard@fr.ibm.com lionel.balmain@fr.ibm.com
 # for email in franck_descollonges@fr.ibm.com lentini@fr.ibm.com courtinb@fr.ibm.com 
+#for email in slebrun@fr.ibm.com
+#for email in bidard@fr.ibm.com
+#for email in gpiedeloup@fr.ibm.com
+for email in courtinb@fr.ibm.com
 
 do
 	## Extract last name from email
@@ -18,11 +22,11 @@ do
 	#ibmcloud account space-create $lastname
 
 	## Invite user and assign role developer to this space
-	ibmcloud account user-invite $email -o cloud-europe -s $lastname --space-role SpaceDeveloper
+	#ibmcloud account user-invite $email -o cloud-europe -s $lastname --space-role SpaceDeveloper
 
     ## Assign Policy
-	#ibmcloud iam user-policy-create $email --roles Administrator --service-name containers-kubernetes
-	#ibmcloud iam user-policy-create $email --roles Administrator --service-name monitoring
-	#ibmcloud iam user-policy-create $email --roles Administrator --service-name ibmcloud-log-analysis 
+	ibmcloud iam user-policy-create $email --roles Administrator --service-name containers-kubernetes
+	ibmcloud iam user-policy-create $email --roles Administrator --service-name monitoring
+	ibmcloud iam user-policy-create $email --roles Administrator --service-name ibmcloud-log-analysis 
 
 done
