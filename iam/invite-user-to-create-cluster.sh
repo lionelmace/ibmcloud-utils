3fac98e80068fe30888b99257cf2e3996ecc21e6
Lionel Mace <lionel.mace@gmail.com>
2018-07-06 13:53:04 +0200

    new script to onboard users

--- iam/invite-user-to-create-cluster.sh
+++ iam/invite-user-to-create-cluster.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-##listofusers = firstname.lastname@fr.ibm.com xx.xxx@gmail.com xx.xxx@gmail.com
 
-# Formation interne Kubernetes
+# Shell script to do the following
+# 1. Extract the last name from the email address
+# 2. Create a Cloud Foundry space with the last name
+# 3. Invite the user into the newly created space
+# 4. Assign policy to the use so he can create a Kube cluster and use both Log Analysis and Monitoring service
+
 for email in firstname.lastname@fr.ibm.com xx.xxx@gmail.com xx.xxx@gmail.com
 
 do
---
+++ iam/remove-user-and-space.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Shell script to remove a list of users and their associated Cloud Foundry space
+for email in firstname.lastname@fr.ibm.com xx.xxx@gmail.com xx.xxx@gmail.com
+
+do
+	## Extract last name from email
+	lastname=$(echo $email | awk -F'@' '{print $1}' | sed 's?.*\.??g' | sed 's?.*\_??g' )
+    echo $lastname
+
+	## Delete Cloud Foundry space with the last name
+	ibmcloud account space-delete $lastname -f
+
+	## Remove User
+	ibmcloud account user-remove $email -f
+done
