ab856af58a3365612382fc0a621cc0d71833025f
Lionel Mace <lionel.mace@gmail.com>
2018-07-06 13:55:48 +0200

    invite

--- iam/invite-user-to-create-cluster.sh
+++ iam/invite-user-to-create-cluster.sh
@@ -9,19 +9,19 @@
 for email in firstname.lastname@fr.ibm.com xx.xxx@gmail.com xx.xxx@gmail.com
 
 do
-	## Extract last name from email
-	lastname=$(echo $email | awk -F'@' '{print $1}' | sed 's?.*\.??g' | sed 's?.*\_??g' )
+  ## Extract last name from email
+  lastname=$(echo $email | awk -F'@' '{print $1}' | sed 's?.*\.??g' | sed 's?.*\_??g' )
 
-	## Create a Cloud Foundry space with the last name
-	echo $lastname
-	ibmcloud account space-create $lastname
+  ## Create a Cloud Foundry space with the last name
+  echo $lastname
+  ibmcloud account space-create $lastname
 
-	## Invite user and assign role developer to this space
-	ibmcloud account user-invite $email -o cloud-europe -s $lastname --space-role SpaceDeveloper
+  ## Invite user and assign role developer to this space
+  ibmcloud account user-invite $email -o cloud-europe -s $lastname --space-role SpaceDeveloper
 
-    ## Assign Policy
-	ibmcloud iam user-policy-create $email --roles Administrator --service-name containers-kubernetes
-	ibmcloud iam user-policy-create $email --roles Administrator --service-name monitoring
-	ibmcloud iam user-policy-create $email --roles Administrator --service-name ibmcloud-log-analysis 
+  ## Assign Policy
+  ibmcloud iam user-policy-create $email --roles Administrator --service-name containers-kubernetes
+  ibmcloud iam user-policy-create $email --roles Administrator --service-name monitoring
+  ibmcloud iam user-policy-create $email --roles Administrator --service-name ibmcloud-log-analysis 
 
 done
