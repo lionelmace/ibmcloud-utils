2db8f30551323eef6f074edb810e7bbde992e009
Lionel Mace <lionel.mace@gmail.com>
2019-05-28 15:11:08 +0200

    fix apikey

--- cloudfunctions/cloud-functions-check-cluster-state.js
+++ cloudfunctions/cloud-functions-check-cluster-state.js
@@ -19,7 +19,7 @@ function main(params) {
              'Content-Type': 'application/x-www-form-urlencoded' },
             form: 
             { grant_type: 'urn:ibm:params:oauth:grant-type:apikey',
-              apikey: '9XwUMh_BFaoLY8o5hi6pKevcmLSwgc-SAC4ou8OjOAzS' } 
+              apikey: '<your-api-key>' } 
         };
       
         request(optionstoken, function (error, response, body) {
@@ -33,7 +33,7 @@ function main(params) {
             method: 'GET',
             url: 'https://eu-central.containers.bluemix.net/v1/clusters/hacluster',
             headers: {
-                Authorization: 'Bearer eyJraWQiOiIyMDE3MTAzMC0wMDowMDowMCIsImFsZyI6IlJTMjU2In0.eyJpYW1faWQiOiJJQk1pZC0yNzAwMDJSMzBBIiwiaWQiOiJJQk1pZC0yNzAwMDJSMzBBIiwicmVhbG1pZCI6IklCTWlkIiwiaWRlbnRpZmllciI6IjI3MDAwMlIzMEEiLCJnaXZlbl9uYW1lIjoiTElPTkVMIiwiZmFtaWx5X25hbWUiOiJNQUNFIiwibmFtZSI6IkxJT05FTCBNQUNFIiwiZW1haWwiOiJsaW9uZWwubWFjZUBmci5pYm0uY29tIiwic3ViIjoibGlvbmVsLm1hY2VAZnIuaWJtLmNvbSIsImFjY291bnQiOnsiYnNzIjoiMGI1YTAwMzM0ZWFmOWViOTMzOWQyYWI0OGY3MzI2YjQiLCJpbXMiOiIxNTk0NTM0In0sImlhdCI6MTU0MzMzMDYwMCwiZXhwIjoxNTQzMzM0MjAwLCJpc3MiOiJodHRwczovL2lhbS5ibHVlbWl4Lm5ldC9pZGVudGl0eSIsImdyYW50X3R5cGUiOiJwYXNzd29yZCIsInNjb3BlIjoiaWJtIG9wZW5pZCIsImNsaWVudF9pZCI6ImJ4IiwiYWNyIjoxLCJhbXIiOlsicHdkIl19.Dg-mPxUH2h7Dst-VlJ1Y50N0VQvegMiMi_HOyw7moUYOcg4MR4KLf3qUT_5JBDMDYAIR1Msdc24uI5LZVGCkKbhiX_CU6N2uo9RsP6Tn3U0n1qBauBYhmKhXq2F-Ko_vjg2Px_gVe5Alz4_ntsNUVtBZG33jWQMVgseDtbQyRFWikCK3bfNwceysuyeHCldpJm-eGuA_nVF7bjPOGLMQO9RrZH1awPUEJ-km1td5ClgDu8PHNKhOpjvYuqSwmiQhmzD0JhXGuBmEQHZUSVR1j_UxRhvQA3lzHPTmmVfHkRVeCevu7sZ_pxzLUUxoh7s84fxCJNiI6lk0N-yi6u7qUg'
+                Authorization: 'Bearer <your-bearer-token>'
             },
             json: true
         };
