223a3c53f15aaff25b774302a9933932f80dca07
Lionel Mace <lionel.mace@gmail.com>
2020-04-16 15:46:17 +0200

    script to cleanup storage

--- classic-infra/storage-block-file-cleanup.sh
+++ classic-infra/storage-block-file-cleanup.sh
@@ -2,11 +2,12 @@
 #
 # Loop to delete file and block storage
 
-source ../local.env
-
-#for i in `ic sl block volume-list | awk '{ print $1}' `
-for i in `ic sl file volume-list | awk '{ print $1}' `
+for i in `ic sl file volume-list | awk '{ print $1 }' `
 do
-  #ic sl block volume-cancel $i -f
   ic sl file volume-cancel $i -f
+done
+
+for i in `ic sl block volume-list | awk '{ print $1 }' `
+do
+  ic sl block volume-cancel $i -f
 done
