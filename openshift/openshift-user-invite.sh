02a0822801f1c26f692bbcaca172fe583a391d3c
Lionel Mace <lionelmace@liphone.home>
2020-12-02 17:28:55 +0100

    cosmetic

--- openshift/openshift-user-invite.sh
+++ openshift/openshift-user-invite.sh
@@ -94,36 +94,36 @@ do
   project_name="lab-$lastname"
 
   # Create a new openshift project with the last name
-  printf "\n## Creating project \"$project_name\" ...\n"
+  printf "\n## Creating project \"$project_name\".\n"
   oc new-project $project_name
 
   # Invite user to the account
-  printf "\n## Inviting user \"$email\" to the account \"$ACCOUNT_ID\" ...\n"
+  printf "\n## Inviting user \"$email\" to the account id \"$ACCOUNT_ID\".\n"
   ibmcloud account user-invite $email
 
   # Assign IAM User Policy - Warning: Those 2 independant policies are required.
-  printf "\n## Assigning IAM Platform Viewer to allow the user to see the cluster in the IBM Cloud environment\n"
+  printf "\n## Assigning IAM Viewer policy to allow the user to view the cluster in the IBM Cloud Console.\n"
   ibmcloud iam user-policy-create $email --roles Viewer --service-name containers-kubernetes --service-instance $CLUSTER_ID
   # Grant admin access on the OpenShift project
-  printf "\n## Assigning IAM Platform Writer to the project\n"
+  printf "\n## Assigning IAM Writer policy to the user project.\n"
   ibmcloud iam user-policy-create $email --roles Writer --service-name containers-kubernetes --service-instance $CLUSTER_ID --attributes "namespace=$project_name"
 
   # Add edit role to the user so they can work within the project $project_name
   # Warning: user will only appear in OpenShift once the user has logged at least once
   # iam_email=$(oc get users | grep -i $email | awk '{ print $1}')
-  printf "\n## Add Edit role to the user $email so he can work within the project $project_name...\n"
+  printf "\n## Add Edit role to the user $email so he can work within the project \"$project_name\".\n"
   # oc adm policy add-role-to-user edit IAM#$email -n $project_name
 
   # Enable user to install Operator from OperatorHub
-  printf "\n## Enabling user to install Operator from OperatorHub...\n"
+  printf "\n## Enabling user to install Operator from OperatorHub.\n"
   enableClusterRoleBinding $lastname $email
   enableClusterRole
   enableRoleBinding $project_name $email
 
   # Access has been granted
-  printf "\n## URL to view the cluster overview in IBM Cloud:\n"
+  printf "\n## Link to access the Cluster Overview in IBM Cloud:\n"
   printf "https://cloud.ibm.com/kubernetes/clusters/$CLUSTER_ID/overview?bss_account=$ACCOUNT_ID"
-  printf "\n## URL to access directly the Openshift console:\n"
+  printf "\n## Link to access directly the Openshift console:\n"
   # URL to access the OpenShift Console and enter credentials
   # Example: https://console-openshift-console.iro-541970-483cccd2f0d38128dd40d2b711142ba9-0000.eu-de.containers.appdomain.cloud/topology/ns/lab-mace
   echo https://console-openshift-console.$INGRESS_URL
