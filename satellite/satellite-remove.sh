4015205354e2b98ccf29a54fed9e19fb7393d42d
Lionel Mace <lionel.mace@gmail.com>
2021-03-18 18:13:53 +0100

    remove satellite

---
+++ satellite/satellite-remove.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source satellite.env
+
+ibmcloud target -g $RESOURCE_GROUP_NAME
+
+# Delete a new Satellite location
+printf "\n## Deleting the Satellite location \"$SAT_LOCATION_NAME\".\n"
+# ibmcloud sat location rm --location $SAT_LOCATION_NAME -f
+
+deleteHosts(){
+  printf "\n## Deleting the VSI instance sat-$SAT_LOCATION_NAME-cp$i...\n"
+  # Retrieve instance id
+  export VSI_INSTANCE_ID=$(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-cp$i | awk '{ print $1 }')
+  ibmcloud is instance-delete $VSI_INSTANCE_ID -f
+}
+
+for i in $(seq -w $COUNT_START $COUNT_END)
+do
+  deleteHosts
+done
+
+printf "\n## ----------------------------------------------------\n"
