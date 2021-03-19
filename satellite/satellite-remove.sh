e8b1bcc928bb5ebe91eb181c41afce64df672eb2
Vincent Perrin <vincent.perrin@fr.ibm.com>
2021-03-19 11:15:36 +0100

    Update satellite-remove.sh
    - Added a function to remove WorkerNodes "deleteHosts_WN"
    - Rename "deleteHosts" function to deleteHosts_CP

--- satellite/satellite-remove.sh
+++ satellite/satellite-remove.sh
@@ -8,16 +8,24 @@ ibmcloud target -g $RESOURCE_GROUP_NAME
 printf "\n## Deleting the Satellite location \"$SAT_LOCATION_NAME\".\n"
 # ibmcloud sat location rm --location $SAT_LOCATION_NAME -f
 
-deleteHosts(){
+deleteHosts_CP(){
   printf "\n## Deleting the VSI instance sat-$SAT_LOCATION_NAME-cp$i...\n"
   # Retrieve instance id
   export VSI_INSTANCE_ID=$(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-cp$i | awk '{ print $1 }')
   ibmcloud is instance-delete $VSI_INSTANCE_ID -f
 }
 
+deleteHosts_WN(){
+  printf "\n## Deleting the VSI instance sat-$SAT_LOCATION_NAME-cp$i...\n"
+  # Retrieve instance id
+  export VSI_INSTANCE_ID=$(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-wn$i | awk '{ print $1 }')
+  ibmcloud is instance-delete $VSI_INSTANCE_ID -f
+}
+
 for i in $(seq -w $COUNT_START $COUNT_END)
 do
-  deleteHosts
+  deleteHosts_CP
+  deleteHosts_WN
 done
 
 printf "\n## ----------------------------------------------------\n"
