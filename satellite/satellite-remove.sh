4ecd4188ffdf335452c9263ff09108fa8e8bc28c
Franck Descollonges <franck_descollonges@fr.ibm.com>
2021-03-22 16:49:08 +0100

    Update satellite-remove.sh

--- satellite/satellite-remove.sh
+++ satellite/satellite-remove.sh
@@ -4,28 +4,67 @@ source satellite.env
 
 ibmcloud target -g $RESOURCE_GROUP_NAME
 
-# Delete a new Satellite location
-printf "\n## Deleting the Satellite location \"$SAT_LOCATION_NAME\".\n"
-# ibmcloud sat location rm --location $SAT_LOCATION_NAME -f
-
-deleteHosts_CP(){
-  printf "\n## Deleting the VSI instance sat-$SAT_LOCATION_NAME-cp$i...\n"
-  # Retrieve instance id
-  export VSI_INSTANCE_ID=$(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-cp$i | awk '{ print $1 }')
-  ibmcloud is instance-delete $VSI_INSTANCE_ID -f
+# Create VSIs for Control Plane
+deleteHostsForControlPlane(){
+  printf "\n############# VSI sat-$SAT_LOCATION_NAME-cp$i ##############\n"
+  ibmcloud is instance-delete --force $(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-cp$i  | awk '{print $1}') 
 }
 
-deleteHosts_WN(){
-  printf "\n## Deleting the VSI instance sat-$SAT_LOCATION_NAME-cp$i...\n"
-  # Retrieve instance id
-  export VSI_INSTANCE_ID=$(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-wn$i | awk '{ print $1 }')
-  ibmcloud is instance-delete $VSI_INSTANCE_ID -f
+# Create VSIs for Worker Node
+deleteHostsForWorkerNode(){
+  printf "\n############# VSI sat-$SAT_LOCATION_NAME-wn$i ##############\n"
+  ibmcloud is instance-delete --force $(ibmcloud is instances | grep sat-$SAT_LOCATION_NAME-wn$i  | awk '{print $1}')
+}
+
+# Assign hosts to the location
+detachControlPlaneFromLocation(){
+  printf "\n############# Detach Controlplane host sat-$SAT_LOCATION_NAME-cp$i ##############\n"
+    ibmcloud sat host rm -f --location $SAT_LOCATION_NAME \
+                        --host sat-$SAT_LOCATION_NAME-cp$i
+}
+
+detachWokerNodeFromLocation(){
+  printf "\n############# Detach Worker host sat-$SAT_LOCATION_NAME-cp$i ##############\n"
+    ibmcloud sat host rm -f --location $SAT_LOCATION_NAME \
+                        --host sat-$SAT_LOCATION_NAME-wn$i
 }
 
 for i in $(seq -w $COUNT_START $COUNT_END)
 do
-  deleteHosts_CP
-  deleteHosts_WN
+  detachControlPlaneFromLocation
+  detachWokerNodeFromLocation
 done
+
+# # Location creation takes few minutes and hosts to be visible in this location
+cp_count=$(ibmcloud sat host ls --location $SAT_LOCATION_NAME | grep "assigned" | wc -l)
+while [ $cp_count -gt 0 ]
+do
+  echo "Waiting for $COUNT_END control plane hosts to be dettached"
+  echo "Number of cp hosts currently attached : $cp_count"
+  cp_count=$(ibmcloud sat host ls --location $SAT_LOCATION_NAME | grep "assigned" | wc -l)
+  sleep 30
+done
+
+# Delete Satellite location
+printf "\n## Delete new Satellite location \"$SAT_LOCATION_NAME\".\n"
+ibmcloud sat location rm -f --location $SAT_LOCATION_NAME
+
+loc_count=$(ibmcloud sat location ls | grep $SAT_LOCATION_NAME-cp | wc -l)
+while [ $cp_count -gt 0 ]
+do
+  echo "Waiting for $COUNT_END control plane hosts to be attached"
+  echo "Number of cp hosts currently attached : $cp_count"
+  loc_count=$(ibmcloud sat location ls | grep $SAT_LOCATION_NAME-cp | wc -l)
+  sleep 30
+done
+
+for i in $(seq -w $COUNT_START $COUNT_END)
+do
+  echo "Deleting cp and wn $i"
+  deleteHostsForControlPlane
+  deleteHostsForWorkerNode
+done
+
+
 
 printf "\n## ----------------------------------------------------\n"
