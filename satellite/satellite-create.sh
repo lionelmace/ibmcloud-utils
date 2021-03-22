367405615411e7f9948030be1b0cecda2d483204
Lionel Mace <lionel.mace@gmail.com>
2021-03-22 22:17:39 +0100

    fix zone names

--- satellite/satellite-create.sh
+++ satellite/satellite-create.sh
@@ -6,7 +6,9 @@ ibmcloud target -g $RESOURCE_GROUP_NAME
 
 # Create a new Satellite location
 printf "\n## Creating new Satellite location \"$SAT_LOCATION_NAME\".\n"
-ibmcloud sat location create --name $SAT_LOCATION_NAME --managed-from $SAT_MANAGED_FROM --ha-zone zone-01 --ha-zone zone-02 --ha-zone zone-03
+ibmcloud sat location create --name $SAT_LOCATION_NAME \
+                             --managed-from $SAT_MANAGED_FROM \
+                             --ha-zone zone-1 --ha-zone zone-2 --ha-zone zone-3
 
 # Retrieve the id of the newly created Satellite location
 export SAT_LOCATION_ID=$(ibmcloud sat location get --location $SAT_LOCATION_NAME --json | jq ".id")
@@ -17,7 +19,7 @@ assign_host_script=$(ibmcloud sat host attach --location $SAT_LOCATION_NAME | gr
 echo $assign_host_script
 
 # Update the Assign Host script to refresh the Red Hat packages on those hosts.
-printf "\n## Updating the Assign host script.\n"
+printf "\n## Updating the Register host script.\n"
 sed -i "" -e $'18 a\
 subscription-manager refresh\\\n' $assign_host_script
 sed -i "" -e $'19 a\
@@ -61,7 +63,7 @@ done
 cp_count=$(ibmcloud sat host ls --location $SAT_LOCATION_NAME | grep $SAT_LOCATION_NAME-cp | wc -l)
 while [ $cp_count -lt $COUNT_END ]
 do
-  echo "Waiting for $COUNT_END control plane hosts to be attached"
+  echo "Waiting 30 secs for $COUNT_END control plane hosts to be attached..."
   echo "Number of cp hosts currently attached : $cp_count"
   cp_count=$(ibmcloud sat host ls --location $SAT_LOCATION_NAME | grep $SAT_LOCATION_NAME-cp | wc -l)
   sleep 30
