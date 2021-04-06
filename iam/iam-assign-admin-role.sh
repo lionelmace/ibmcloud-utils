6623f4dcce2b25cb5170ebbb6f006b6fbaf897ef
Lionel Mace <lionel.mace@gmail.com>
2021-04-06 14:53:59 +0200

    admin iam

--- iam/iam-assign-admin-role.sh
+++ iam/iam-assign-admin-role.sh
@@ -5,23 +5,48 @@
 ## 2. All resources in account (including future IAM enabled services)
 ## 3. All User Management resources
 
-for email in first.lastname@company.com
+for email in first.last@company.com
 
 do
   # List the current user policies
   echo $email
-  # ibmcloud iam user-policies $email
+  # ibmcloud iam user-policies $email --output json
+
+  # ---------------------------------------------------------------------------------------
+  # Account Management
+  # ---------------------------------------------------------------------------------------
+
+  # Ability to invite Users - Minimum role: Editor
+  ibmcloud iam user-policy-create $email --roles Editor --service-name user-management
+
+  # Ability to create Access Group - Minimum role: Editor
+  ibmcloud iam user-policy-create $email --roles Editor --service-name iam-groups
+
+  # Ability to assign policy to a User - Minimum role: Administrator
+  ibmcloud iam user-policy-create $email --roles Administrator --service-name iam-svcs
+
+
+  # ---------------------------------------------------------------------------------------
+  # IAM Services
+  # ---------------------------------------------------------------------------------------
+
+  # Ability to administrate all resources in the account
+  ibmcloud iam user-policy-create $email --roles Administrator
+
+
+  # Ability to assign policy to a Resource Group - Minimum role: Administrator
+  # ibmcloud iam user-policy-create $email --roles Administrator --resource-type resource-group
 
   # Add Policy: All account management services
-  ibmcloud iam user-policy-create $email --roles Administrator --service-type platform_service
+  # ibmcloud iam user-policy-create $email --roles Administrator --service-type platform_service
   
   # Add Policy: All Identity and Access enabled services
   # Become
   # All resources in account (including future IAM enabled services)
   # All resources in ale-watson resource group
-  ibmcloud iam user-policy-create $email --roles Administrator,Manager --service-type service
+  #ibmcloud iam user-policy-create $email --roles Administrator,Manager --service-type service
 
   # Add Policy: All User Management resources
-  ibmcloud iam user-policy-create $email --roles Administrator --service-name user-management 
+  # ibmcloud iam user-policy-create $email --roles Administrator --service-name user-management 
 
 done
