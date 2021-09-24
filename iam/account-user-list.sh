e1f44c8f9be840511757e2343949d24aa243b5ad
Lionel Mace <lionel.mace@fr.ibm.com>
2021-09-24 11:35:50 +0200

    minor change

--- iam/account-user-list.sh
+++ iam/account-user-list.sh
@@ -3,25 +3,27 @@
 source ../local.env
 
 count=0
-file=account_rh_users.txt
+file=account_ibm_users.txt
 # Empty file content
 cat /dev/null > $file
 
 for i in `ibmcloud account users | awk '{ print $1}' `
 do
   # Extract all IBM Cloud Users
-  # ALL=$ALL$i,
-  # ((count=count+1))
-
-  # Extract only Red Hat users
-  if [[ $i == *"redhat"* ]]; then
+  if [[ $i == *"ibm"* ]]; then
     ALL=$ALL$i,
     ((count=count+1))
   fi
+
+  # Extract only Red Hat users
+  # if [[ $i == *"redhat"* ]]; then
+  #   ALL=$ALL$i,
+  #   ((count=count+1))
+  # fi
   
 done
 
-echo $count 'users extracted to $file'
+echo $count 'users extracted to ' $file
 echo $ALL >> $file
 cat $file
 
