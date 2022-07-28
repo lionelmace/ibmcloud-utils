64ad2d984bb53b9e68150433e4fea91f946fabb7
Lionel Mace <lionel.mace@gmail.com>
2022-07-28 12:00:23 -0400

    added tf for iam toolchain

--- terraform/poc-openshift/iam.tf
+++ terraform/poc-openshift/iam.tf
@@ -24,12 +24,24 @@ resource "ibm_iam_access_group_policy" "policy-k8s" {
   }
 }
 
+# DevOps - Continuous Delivery
 resource "ibm_iam_access_group_policy" "iam-continuous-delivery" {
   access_group_id = ibm_iam_access_group.accgrp.id
   roles           = ["Manager", "Writer", "Editor", "Operator", "Viewer"]
 
   resources {
     service           = "continuous-delivery"
+    resource_group_id = ibm_resource_group.resource_group.id
+  }
+}
+
+# DevOps - Toolchain
+resource "ibm_iam_access_group_policy" "iam-continuous-delivery" {
+  access_group_id = ibm_iam_access_group.accgrp.id
+  roles           = ["Editor", "Operator", "Viewer"]
+
+  resources {
+    service           = "toolchain"
     resource_group_id = ibm_resource_group.resource_group.id
   }
 }
