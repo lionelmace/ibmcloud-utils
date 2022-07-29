da8006c5866c6d7a42fc6675f816ebf7c659db02
Lionel Mace <lionel.mace@gmail.com>
2022-07-29 17:20:38 +0200

    removed email from terraform

--- terraform/poc-openshift/invite-users.tf
+++ terraform/poc-openshift/invite-users.tf
@@ -1,4 +1,4 @@
 resource "ibm_iam_user_invite" "invite_user" {
-  users         = ["aurelien.gravier@phenisys.com","frederic.barlet@eovi-mcd.fr","jean.blanc@phenisys.com","julien.egron@phenisys.com"]
+  users         = ["first.lastname@ibm.com"]
   access_groups = [ibm_iam_access_group.accgrp.id]
 }
