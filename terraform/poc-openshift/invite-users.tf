6667bf9e287dcf00e1a801ec24952b65a235f1a5
Lionel Mace <lionel.mace@gmail.com>
2022-07-29 12:36:48 +0200

    added email back

--- terraform/poc-openshift/invite-users.tf
+++ terraform/poc-openshift/invite-users.tf
@@ -1,4 +1,4 @@
 resource "ibm_iam_user_invite" "invite_user" {
-  users         = ["first.lastname@company.fr"]
+  users         = ["aurelien.gravier@phenisys.com","frederic.barlet@eovi-mcd.fr","jean.blanc@phenisys.com","julien.egron@phenisys.com"]
   access_groups = [ibm_iam_access_group.accgrp.id]
 }
