97eab394d09163bf5ed066a78bb8897cda20272b
Lionel Mace <lionel.mace@gmail.com>
2022-10-06 20:39:37 +0200

    change secrets manager plan

--- secrets-manager/attach-sm-cluster.sh
+++ secrets-manager/attach-sm-cluster.sh
@@ -5,14 +5,24 @@
 
 source ./local.env
 
-#ibmcloud login -g $RG_NAME -r $IBMCLOUD_REGION --apikey $APIKEY
-
-ibmcloud oc cluster get --cluster $CLUSTER_ID | grep Ingress
-
-ibmcloud oc ingress secret ls -c $CLUSTER_ID
-
+ibmcloud login -g $RG_NAME -r $IBMCLOUD_REGION --apikey $APIKEY
 
 export SECRETS_MANAGER_URL=https://$SM_ID.$IBMCLOUD_REGION.secrets-manager.appdomain.cloud
+
+# attach the secrets manager instance to the cluster
+echo "Attaching the secrets manager..."
+ibmcloud ks ingress instance register \
+    --cluster $CLUSTER_ID \
+    --crn $SECRETS_MANAGER_ID \
+    --is-default
+
+# Verify that the Secrets Manager instance was registered to the cluster. 
+ibmcloud ks ingress instance ls --cluster $CLUSTER_ID
+
+# ibmcloud oc cluster get --cluster $CLUSTER_ID | grep Ingress
+
+# ibmcloud oc ingress secret ls -c $CLUSTER_ID
+
 
 # Create a Secrets Group
 # ibmcloud secrets-manager secret-group-create \
@@ -24,13 +34,14 @@ export SECRETS_MANAGER_URL=https://$SM_ID.$IBMCLOUD_REGION.secrets-manager.appdo
 #                                         | jq -r '.resources.id')
 # echo $SM_GROUP_ID
 
-ibmcloud secrets-manager secret-create \
-    --secret-type=arbitrary \
-    --resources='[{"name": "example-arbitrary-secret", "description": "Extended description for this secret.", "labels": ["dev","us-south"], "custom_metadata": {"anyKey": "anyValue"}, "version_custom_metadata": {"anyKey": "anyValue"}, "expiration_date": "2030-01-01T00:00:00Z", "payload": "secret-data"}]'
+# Create a secret
+# ibmcloud secrets-manager secret-create \
+#     --secret-type=arbitrary \
+#     --resources='[{"name": "example-arbitrary-secret", "description": "Extended description for this secret.", "labels": ["dev","us-south"], "custom_metadata": {"anyKey": "anyValue"}, "version_custom_metadata": {"anyKey": "anyValue"}, "expiration_date": "2030-01-01T00:00:00Z", "payload": "secret-data"}]'
     # --resources='[{"name": "example-arbitrary-secret", "description": "Extended description for this secret.", "secret_group_id": "bc656587-8fda-4d05-9ad8-b1de1ec7e712", "labels": ["dev","us-south"], "custom_metadata": {"anyKey": "anyValue"}, "version_custom_metadata": {"anyKey": "anyValue"}, "expiration_date": "2030-01-01T00:00:00Z", "payload": "secret-data"}]'
 
 # List SM groups
-ibmcloud secrets-manager secret-groups
+# ibmcloud secrets-manager secret-groups
 
 # Step 1 - Run the command to set the new default instance.
 # You can optionally specify a secret group that is allowed access to the secrets in the instance.
--- terraform/poc-openshift/secrets-manager.tf
+++ terraform/poc-openshift/secrets-manager.tf
@@ -4,7 +4,7 @@
 resource "ibm_resource_instance" "secrets-manager" {
   name              = "${var.prefix}-secrets-manager"
   service           = "secrets-manager"
-  plan              = "trial"
+  plan              = "standard"
   location          = var.region
   resource_group_id = ibm_resource_group.resource_group.id
   tags              = var.tags
