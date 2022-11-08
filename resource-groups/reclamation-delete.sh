282a0b0582cced9ee2d1325fb2c0fe509d887bca
Lionel Mace <lionel.mace@gmail.com>
2022-11-08 19:30:21 +0100

    fix reclamations

--- resource-groups/reclamation-delete.sh
+++ resource-groups/reclamation-delete.sh
@@ -6,7 +6,8 @@ export RG_NAME="mytodo"
 
 ibmcloud target -g $RG_NAME
 
-for rr in $(ibmcloud resource reclamations | awk '{print $1}')
+# Start reading at 4th line
+for rr in $(ibmcloud resource reclamations | awk '(NR>4) {print $1}')
 do
   #echo $rr
   ibmcloud resource reclamation-delete $rr -f
