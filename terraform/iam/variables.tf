13224c4e4a4c5b0d610dd7889f56ebf09301eec8
Lionel Mace <lionel.mace@gmail.com>
2022-12-03 08:25:27 +0100

    remove tf iam

--- terraform/iam/variables.tf
+++
@@ -1,9 +0,0 @@
-
-variable "ibmcloud_api_key" {
-  description = "The IBM Cloud platform API key needed to deploy IAM enabled resources"
-}
-
-variable "region" {
-  description = "IBM Cloud region where all resources will be provisioned"
-  default     = ""
-}
