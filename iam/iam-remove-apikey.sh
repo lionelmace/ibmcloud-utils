904e4d516a153f530c475ce66d09fbdd91b68c09
Lionel Mace <lionel.mace@gmail.com>
2022-12-16 18:57:19 +0100

    remove api key of clusters

---
+++ iam/iam-remove-apikey.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Remove the API Key created when provisioning clusters
+# but don't get removed when deleting clusters!!
+
+source ../local.env
+
+count=0
+
+for i in `ibmcloud iam api-keys | grep containers-kubernetes-key | awk 'NR>5 { print $NF}' `
+do
+  # Delete API Key
+  ibmcloud iam api-key-delete $i -f
+  count=count+1
+  
+done
+
+echo $count 'api keys deleted'
