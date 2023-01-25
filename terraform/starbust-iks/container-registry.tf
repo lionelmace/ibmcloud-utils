3009607ed0ee828d8a5d791927836c327f7ac3dd
Lionel Mace <lionel.mace@gmail.com>
2023-01-25 11:49:34 +0100

    updated terraform registry

--- terraform/starbust-iks/container-registry.tf
+++ terraform/starbust-iks/container-registry.tf
@@ -1,4 +1,4 @@
-resource "ibm_cr_namespace" "container-registry-namespace" {
+resource "ibm_cr_namespace" "registry-namespace" {
   name              = format("%s-%s", var.prefix, "registry")
   resource_group_id = ibm_resource_group.resource_group.id
   tags              = var.tags
@@ -14,5 +14,7 @@ resource "ibm_iam_access_group_policy" "iam-registry" {
   resources {
     service           = "container-registry"
     resource_group_id = ibm_resource_group.resource_group.id
+    resource_type     = "namespace"
+    resource          = ibm_cr_namespace.registry-namespace.name
   }
 }
