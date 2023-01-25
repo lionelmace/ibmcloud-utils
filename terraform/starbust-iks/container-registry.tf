bc0d07ab70617591bc31417d0360bab8f241f071
Lionel Mace <lionel.mace@gmail.com>
2023-01-25 11:11:59 +0100

    added registry IAM

--- terraform/starbust-iks/container-registry.tf
+++ terraform/starbust-iks/container-registry.tf
@@ -3,3 +3,16 @@ resource "ibm_cr_namespace" "container-registry-namespace" {
   resource_group_id = ibm_resource_group.resource_group.id
   tags              = var.tags
 }
+
+## IAM
+##############################################################################
+# Role Writer supports both Pull and Push
+resource "ibm_iam_access_group_policy" "iam-registry" {
+  access_group_id = ibm_iam_access_group.accgrp.id
+  roles           = ["Viewer", "Writer"]
+
+  resources {
+    service           = "container-registry"
+    resource_group_id = ibm_resource_group.resource_group.id
+  }
+}
