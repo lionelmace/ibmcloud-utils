f78c9dc9a943f18eb2ed5f6ca92a25f118f5424c
Lionel Mace <lionel.mace@gmail.com>
2023-02-21 14:13:43 +0100

    removed starbust from this git

--- terraform/starbust-iks/resource-group.tf
+++
@@ -1,8 +0,0 @@
-
-##############################################################################
-# Resource Group where Cloud Resources will be created
-##############################################################################
-
-resource "ibm_resource_group" "resource_group" {
-  name = var.resource_group
-}
