4a0de38794427ee5010ca9996afc530f5351063d
Lionel Mace <lionel.mace@gmail.com>
2023-06-26 21:31:09 +0200

    added script to remove volumes

---
+++ infrastructure/is-remove-vols.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Remove the Block Storage volumes after ODF de-installation
+
+count=0
+
+for v in `ibmcloud is vols | grep odf | awk 'NR>1 {print $1}' `
+do
+  # Delete API Key
+  ibmcloud is vold $v -f
+  ((count=count+1))
+  
+done
+
+echo $count 'volumes deleted'
