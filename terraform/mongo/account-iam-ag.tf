aedea8c0bc1fe78c5cd9ef0adf9ada4f16956903
Lionel Mace <lionel.mace@gmail.com>
2023-09-13 13:33:13 +0200

    remove comment

--- terraform/mongo/account-iam-ag.tf
+++ terraform/mongo/account-iam-ag.tf
@@ -1,9 +1,3 @@
-# Invite users to the Access Group
-# resource "ibm_iam_user_invite" "invite_user" {
-#   users         = var.emails
-#   depends_on    = [ibm_iam_access_group.accgrp]
-# }
-
 # Create Access Group
 resource "ibm_iam_access_group" "accgrp" {
   name = format("%s-%s", local.basename, "ag")
