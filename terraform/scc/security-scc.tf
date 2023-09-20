5e766ab5f7489484c4fb6835c0ea71cb155a5dcf
Lionel Mace <lionel.mace@gmail.com>
2023-09-20 15:42:44 +0200

    clean up scc in terraform

--- terraform/scc/security-scc.tf
+++ terraform/scc/security-scc.tf
@@ -1,25 +1,50 @@
+# Account ID is required for CBR Rule and Zone
+data "ibm_iam_account_settings" "account_settings" {
+}
+
+locals {
+  account_id = data.ibm_iam_account_settings.account_settings.account_id
+}
+
+## SCC Instance
+##############################################################################
+resource "ibm_resource_instance" "scc_instance" {
+  name              = format("%s-%s", local.basename, "scc")
+  service           = "compliance"
+  plan              = "security-compliance-center-standard-plan"
+  location          = var.region
+  resource_group_id = ibm_resource_group.group.id
+}
+
 ## SCC Profile Attachment
 ##############################################################################
 resource "ibm_scc_profile_attachment" "scc_profile_attachment_instance" {
-  # CIS IBM Foundations Benchmark v1.0.0
   name       = format("%s-%s", local.basename, "cis")
-  profile_id = "a0bd1ee2-1ed3-407e-a2f4-ce7a1a38f54d"
+  profile_id = "a0bd1ee2-1ed3-407e-a2f4-ce7a1a38f54d" # CIS IBM Foundations v1.0.0
   scope {
-    environment = ""
+    environment = "ibm-cloud"
     properties {
-      name = ""
-      value = ""
+      name = "scope_id"
+      value = local.account_id
     }
-  }
-  notifications {
-    controls {
-      failed_control_ids = [""]
-      threshold_limit = 0
+    properties {
+      name  = "scope_type"
+      value = "account"
     }
-    enabled = false
+    # properties {
+    #   name = "exclusions"
+    #   value = []
+    # }
   }
   schedule = "daily"
   status   = "enabled"
+  notifications {
+    enabled = false
+    controls {
+      failed_control_ids = []
+      threshold_limit    = 14
+    }
+  }
 }
 
 
