1f5ee48d4e4f2c0d7733b24daeb7f05767950542
Lionel Mace <lionel.mace@gmail.com>
2023-09-29 18:37:22 +0200

    upgrade to 1.58.0 tf provider

--- terraform/scc/provider.tf
+++ terraform/scc/provider.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "1.58.0-beta1"
+      version = "1.58.0"
     }
     http-full = {
       source = "salrashid123/http-full"
