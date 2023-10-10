64972104779d9ed27f4dc5c4e644bbd59d32ebf4
Lionel Mace <lionel.mace@gmail.com>
2023-10-10 17:36:54 +0200

    added workaround for cis certificate order

--- terraform/internet-services/internet-services.tf
+++ terraform/internet-services/internet-services.tf
@@ -8,12 +8,12 @@ data "ibm_cis_domain" "cis_instance_domain" {
   cis_id = data.ibm_cis.cis_instance.id
 }
 
-resource "ibm_cis_certificate_order" "test" {
-    cis_id    = data.ibm_cis.cis_instance.id
-    domain_id = data.ibm_cis_domain.cis_instance_domain.domain_id
-    hosts     = ["test.lionelmace.com"]
-    # certificate_authority = "lets_encrypt"
-}
+# resource "ibm_cis_certificate_order" "test" {
+#     cis_id    = data.ibm_cis.cis_instance.id
+#     domain_id = data.ibm_cis_domain.cis_instance_domain.domain_id
+#     hosts     = ["test.lionelmace.com"]
+#     # certificate_authority    = "lets_encrypt"
+# }
 
 output "cis_instance_name" {
   value = data.ibm_cis.cis_instance.name
@@ -21,4 +21,31 @@ output "cis_instance_name" {
 
 output "cis_instance_status" {
   value = data.ibm_cis_domain.cis_instance_domain.status
+}
+
+## Workaround to connect a COS bucket to the SCC instance
+##############################################################################
+data "ibm_iam_auth_token" "tokendata" {}
+
+data "http" "cis_certificate_order" {
+  provider = http-full
+
+  url    = "https://api.cis.cloud.ibm.com/v2/${data.ibm_cis.cis_instance.id}/zones/${data.ibm_cis_domain.cis_instance_domain.domain_id}/ssl/certificate_packs/order"
+  method = "POST"
+
+  request_headers = {
+    x-auth-user-token = data.ibm_iam_auth_token.tokendata.iam_access_token
+    content-type      = "application/json"
+    accept            = "application/json"
+  }
+
+  request_body = jsonencode(
+    {
+        type = "advanced"
+        hosts = ["test.lionelmace.com"]
+        validation_method = "txt"
+        validity_days = 90
+        certificate_authority = "lets_encrypt"
+    }
+  )
 }
