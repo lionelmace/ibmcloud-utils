900bae7a9a8223a25bc0fdf4402b4cd57cc80715
Lionel Mace <lionel.mace@gmail.com>
2023-10-17 14:56:16 +0200

    added description to ibm_scc_profile_attachment

--- terraform/scc/security-scc.tf
+++ terraform/scc/security-scc.tf
@@ -12,9 +12,10 @@ resource "ibm_resource_instance" "scc_instance" {
 ## SCC Profile Attachment
 ##############################################################################
 resource "ibm_scc_profile_attachment" "scc_profile_attachment_instance" {
-  name       = format("%s-%s", local.basename, "cis")
-  profile_id = "a0bd1ee2-1ed3-407e-a2f4-ce7a1a38f54d" # CIS IBM Foundations v1.0.0
+  name        = format("%s-%s", local.basename, "cis")
+  profile_id  = "a0bd1ee2-1ed3-407e-a2f4-ce7a1a38f54d" # CIS IBM Foundations v1.0.0
   instance_id = ibm_resource_instance.scc_instance.guid
+  description = "My SCC Profile Attachment"
   scope {
     environment = "ibm-cloud"
     properties {
