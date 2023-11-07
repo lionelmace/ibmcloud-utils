d714ac07ff92fe03528f29cffdc789015c46879b
Lionel Mace <lionel.mace@gmail.com>
2023-11-07 14:36:01 +0100

    updated user to access group

--- iam/invite-users-to-access-group.sh
+++ iam/invite-users-to-access-group.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
-## Access Group Name
-AG="workshop"
+ACCESS_GROUP_NAME="workshop"
 
 for email in $EMAIL
 
 do
-  ibmcloud iam access-group-user-add $AG $email
+  # Invite user to the Account
+  printf "\n## Inviting user \"$email\" to the account id \"$ACCOUNT_ID\".\n"
+  ibmcloud account user-invite $email
+
+  # Invite user to the IAM Access Group
+  printf "\n## Inviting user \"$email\" to the Acces Group \"$ACCESS_GROUP_NAME\".\n"
+  ibmcloud iam access-group-user-add $ACCESS_GROUP_NAME $email
+
 done
