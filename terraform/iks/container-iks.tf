0a9bd50a65c11e82f38c7f2ab000aa6fb5dad069
Lionel Mace <lionel.mace@gmail.com>
2023-11-14 10:35:10 +0100

    rename prefix for iks

--- terraform/iks/container-iks.tf
+++ terraform/iks/container-iks.tf
@@ -115,14 +115,6 @@ resource "ibm_container_vpc_cluster" "iks_cluster" {
 #   }
 # }
 
-data "ibm_container_vpc_alb" "iks_cluster_alb" {
-  alb_id = ibm_container_vpc_cluster.iks_cluster.albs[0].id
-}
-
-output "iks_cluster_alb" {
-  value = data.ibm_container_vpc_alb.iks_cluster_alb
-}
-
 ##############################################################################
 # Connect Log Analysis Service to cluster
 # 
