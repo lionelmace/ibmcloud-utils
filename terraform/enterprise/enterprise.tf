0b876f9b0adea754d0e6f176e0754a411733fc20
Lionel Mace <lionel.mace@gmail.com>
2023-11-22 18:58:25 +0100

    simplify

--- terraform/enterprise/enterprise.tf
+++ terraform/enterprise/enterprise.tf
@@ -2,12 +2,12 @@ data "ibm_enterprises" "enterprise" {
     name = "Enterprise Account"
 }
 
-resource "ibm_enterprise_account" "enterprise_account" {
-  parent = data.ibm_enterprises.enterprise.id
-  name = "new-child-tf-account"
-  owner_iam_id = "lionel.mace@fr.ibm.com"
-  traits {
-    mfa = "NONE"
-    enterprise_iam_managed = true
-  }
-}
+# resource "ibm_enterprise_account" "enterprise_account" {
+#   parent = data.ibm_enterprises.enterprise.id
+#   name = "new-child-tf-account"
+#   owner_iam_id = "lionel.mace@fr.ibm.com"
+#   traits {
+#     mfa = "NONE"
+#     enterprise_iam_managed = true
+#   }
+# }
