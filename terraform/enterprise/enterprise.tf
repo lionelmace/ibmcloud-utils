924aae8a3e724b548d1d22ac2e957c8ac2cdf76a
Lionel Mace <lionel.mace@gmail.com>
2023-11-22 17:44:45 +0100

    fix owner_iam_id

--- terraform/enterprise/enterprise.tf
+++ terraform/enterprise/enterprise.tf
@@ -5,7 +5,7 @@ data "ibm_enterprises" "enterprise" {
 resource "ibm_enterprise_account" "enterprise_account" {
   parent = data.ibm_enterprises.enterprise.id
   name = "new-child-tf-account"
-  owner_iam_id = data.ibm_enterprises.enterprise.owner_iam_id
+  owner_iam_id = "lionel.mace@fr.ibm.com"
   traits {
     mfa = "NONE"
     enterprise_iam_managed = true
