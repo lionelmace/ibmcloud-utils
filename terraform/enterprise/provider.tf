eed9c5eca67a6f01fe0c4396a5cd006ace4af972
Lionel Mace <lionel.mace@gmail.com>
2023-11-22 17:21:08 +0100

    added enterprise tf

---
+++ terraform/enterprise/provider.tf
@@ -0,0 +1,26 @@
+##############################################################################
+# IBM Cloud Provider
+##############################################################################
+
+terraform {
+  required_version = ">=1.5"
+  required_providers {
+    ibm = {
+      source  = "IBM-Cloud/ibm"
+      version = "1.59.1"
+    }
+  }
+}
+
+provider "ibm" {
+  alias            = "child"
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+}
+
+provider "ibm" {
+  alias            = "enterprise"
+  ibmcloud_api_key = var.ibmcloud_api_key
+  region           = var.region
+}
+##############################################################################
