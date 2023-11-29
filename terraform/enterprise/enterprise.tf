43f131726a7a2e2b1c6dc7798ca8947ef3a2acab
Lionel Mace <lionel.mace@gmail.com>
2023-11-29 18:02:15 +0100

    hhhjk

--- terraform/enterprise/enterprise.tf
+++ terraform/enterprise/enterprise.tf
@@ -7,7 +7,7 @@ data "ibm_enterprise_accounts" "accounts" {
 }
 
 resource "ibm_enterprise_account" "enterprise_account_instance" {
-  parent = data.ibm_enterprise_accounts.accounts.0.crn
+  parent = data.ibm_enterprise_accounts.accounts.accounts.0.crn
   name = "new-child-tf-account"
   owner_iam_id = "lionel.mace@fr.ibm.com"
   traits {
