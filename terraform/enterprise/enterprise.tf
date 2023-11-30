5af8eff4f2ef39589c51229086feb7345f80c87b
Lionel Mace <lionel.mace@gmail.com>
2023-11-30 08:10:03 +0100

    added crn

--- terraform/enterprise/enterprise.tf
+++ terraform/enterprise/enterprise.tf
@@ -6,12 +6,16 @@ data "ibm_enterprise_accounts" "accounts" {
     name = "enterprise-account"
 }
 
-resource "ibm_enterprise_account" "enterprise_account_instance" {
-  parent = data.ibm_enterprise_accounts.accounts.accounts.0.crn
-  name = "new-child-tf-account"
-  owner_iam_id = "lionel.mace@fr.ibm.com"
-  traits {
-    mfa = "NONE"
-    enterprise_iam_managed = true
-  }
+output "enterprise-account-crn" {
+  value = data.ibm_enterprise_accounts.accounts.accounts.0.crn
 }
+
+# resource "ibm_enterprise_account" "enterprise_account_instance" {
+#   parent = data.ibm_enterprise_accounts.accounts.accounts.0.crn
+#   name = "new-child-tf-account"
+#   owner_iam_id = "lionel.mace@fr.ibm.com"
+#   traits {
+#     mfa = "NONE"
+#     enterprise_iam_managed = true
+#   }
+# }
