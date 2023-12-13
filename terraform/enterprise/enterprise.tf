9869cf13eafc539b0951649b296f4d550cf26bbc
Lionel Mace <lionel.mace@gmail.com>
2023-12-13 14:25:52 +0100

    add child account

--- terraform/enterprise/enterprise.tf
+++ terraform/enterprise/enterprise.tf
@@ -15,15 +15,15 @@ data "ibm_enterprise_accounts" "accounts" {
 output "enterprise-account-crn" {
   value = data.ibm_enterprise_accounts.accounts.accounts.0.crn
 }
-# enterprise-account-crn = "crn:v1:bluemix:public:enterprise::a/82e850d55a814f729c69ab4667d0cdf8::account:82e850d55a814f729c69ab4667d0cdf8"
 
-# resource "ibm_enterprise_account" "child_account" {
-#   parent = data.ibm_enterprise_accounts.accounts.accounts.0.crn
-#   # parent = "86d12027dc2245559daf98ce3d130998"
-#   name = "new-child-tf-account"
-#   owner_iam_id = "first.last@fr.ibm.com"
-#   traits {
-#     mfa = "NONE"
-#     enterprise_iam_managed = true
-#   }
-# }
+resource "ibm_enterprise_account" "child_account" {
+  parent = data.ibm_enterprise_accounts.accounts.accounts.0.crn
+  # parent = "86d12027dc2245559daf98ce3d130998"
+  name = "new-child-tf-account"
+  owner_iam_id = "first.last@fr.ibm.com"
+  # Optional:
+  # traits {
+  #   mfa = "NONE"
+  #   enterprise_iam_managed = true
+  # }
+}
