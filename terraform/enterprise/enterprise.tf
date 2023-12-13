1713fe6dbb21658c4696c7409a4bc62d99209211
Lionel Mace <lionel.mace@gmail.com>
2023-12-13 14:43:08 +0100

    try again

--- terraform/enterprise/enterprise.tf
+++ terraform/enterprise/enterprise.tf
@@ -1,11 +1,11 @@
 # Top level Enterprise Account with label "Enterprise" in the Console
-# data "ibm_enterprises" "top-enterprise" {
-#     name = "enterprise-account"
-# }
+data "ibm_enterprises" "top-enterprise" {
+    # name = "enterprise-account"
+}
 
-# output "enterprise-crn" {
-#   value = data.ibm_enterprises.top-enterprise.enterprises.0.crn
-# }
+output "top-enterprise-crn" {
+  value = data.ibm_enterprises.top-enterprise.enterprises.0.crn
+}
 
 # Enterprise account where resources can be provisioned.
 data "ibm_enterprise_accounts" "enterprise-account" {
@@ -19,7 +19,6 @@ output "enterprise-account-crn" {
 output "is_enterprise_account" {
   value = data.ibm_enterprise_accounts.enterprise-account.accounts.0.is_enterprise_account
 }
-
 
 # resource "ibm_enterprise_account" "child_account" {
 #   parent = data.ibm_enterprise_accounts.accounts.accounts.0.crn
