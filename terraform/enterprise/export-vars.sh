b49e68309ef620c69c50794ffc7ea6e17c83370f
Lionel Mace <lionel.mace@gmail.com>
2023-12-15 16:26:30 +0100

    create private cluster

---
+++ terraform/enterprise/export-vars.sh
@@ -0,0 +1,19 @@
+#!/bin/sh	
+# Uncommment to verbose	
+# set -x 	
+
+export IBMCLOUD_ACCOUNT_MANAGEMENT_API_ENDPOINT=https://accountmanagement.test.cloud.ibm.com 	
+export IBMCLOUD_CS_API_ENDPOINT=https://containers.test.cloud.ibm.com/global	
+export IBMCLOUD_CR_API_ENDPOINT=https://registry.test.cloud.ibm.com	
+export IBMCLOUD_CIS_API_ENDPOINT=https://api.cis.test.cloud.ibm.com	
+export IBMCLOUD_GS_API_ENDPOINT=https://api.global-search-tagging.test.cloud.ibm.com	
+export IBMCLOUD_IAMPAP_API_ENDPOINT=https://iam.test.cloud.ibm.com	
+export IBMCLOUD_ICD_API_ENDPOINT=https://api.us-south.databases.test.cloud.ibm.com	
+export IBMCLOUD_UAA_ENDPOINT=https://login.test.cloud.ibm.com/UAALoginServerWAR	
+export IBMCLOUD_COS_ENDPOINT=https://s3.eu-de.cloud-object-storage.test.appdomain.cloud	
+export IBMCLOUD_COS_CONFIG_ENDPOINT=https://config.cloud-object-storage.test.cloud.ibm.com/v1	
+export IBMCLOUD_IAM_API_ENDPOINT=https://iam.test.cloud.ibm.com	
+export IBMCLOUD_RESOURCE_CONTROLLER_API_ENDPOINT=https://resource-controller.test.cloud.ibm.com	
+export IBMCLOUD_RESOURCE_CATALOG_API_ENDPOINT=https://globalcatalog.test.cloud.ibm.com	
+export IBMCLOUD_RESOURCE_MANAGEMENT_API_ENDPOINT=https://resource-controller.test.cloud.ibm.com	
+export IBMCLOUD_GT_API_ENDPOINT=https://tags.global-search-tagging.test.cloud.ibm.com
