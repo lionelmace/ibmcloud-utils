354aa662c654392744ed0c1b40863905cb6fe9c6
Lionel Mace <lionel.mace@gmail.com>
2023-12-15 19:41:46 +0100

    added worker pool

--- terraform/private-roks-vpn/container-openshift.tf
+++ terraform/private-roks-vpn/container-openshift.tf
@@ -91,15 +91,15 @@ variable "worker_pools" {
   }))
 
   default = [
+    # {
+    #   pool_name        = "dev"
+    #   machine_type     = "bx2.4x16"
+    #   workers_per_zone = 1
+    # },
     {
-      pool_name        = "dev"
-      machine_type     = "bx2.4x16"
+      pool_name        = "odf"
+      machine_type     = "bx2.16x64"
       workers_per_zone = 1
-      },
-      {
-          pool_name        = "odf"
-          machine_type     = "bx2.16x64"
-          workers_per_zone = 1
     }
   ]
 
@@ -155,23 +155,23 @@ resource "ibm_container_vpc_cluster" "roks_cluster" {
 
 # Additional Worker Pool
 ##############################################################################
-# resource "ibm_container_vpc_worker_pool" "roks_worker_pools" {
-#   for_each          = { for pool in var.worker_pools : pool.pool_name => pool }
-#   cluster           = ibm_container_vpc_cluster.roks_cluster.id
-#   resource_group_id = ibm_resource_group.group.id
-#   worker_pool_name  = each.key
-#   flavor            = lookup(each.value, "machine_type", null)
-#   vpc_id            = ibm_is_vpc.vpc.id
-#   worker_count      = each.value.workers_per_zone
+resource "ibm_container_vpc_worker_pool" "roks_worker_pools" {
+  for_each          = { for pool in var.worker_pools : pool.pool_name => pool }
+  cluster           = ibm_container_vpc_cluster.roks_cluster.id
+  resource_group_id = ibm_resource_group.group.id
+  worker_pool_name  = each.key
+  flavor            = lookup(each.value, "machine_type", null)
+  vpc_id            = ibm_is_vpc.vpc.id
+  worker_count      = each.value.workers_per_zone
 
-#   dynamic "zones" {
-#     for_each = { for subnet in ibm_is_subnet.subnet : subnet.id => subnet }
-#     content {
-#       name      = zones.value.zone
-#       subnet_id = zones.value.id
-#     }
-#   }
-# }
+  dynamic "zones" {
+    for_each = { for subnet in ibm_is_subnet.subnet : subnet.id => subnet }
+    content {
+      name      = zones.value.zone
+      subnet_id = zones.value.id
+    }
+  }
+}
 
 
 ## IAM
