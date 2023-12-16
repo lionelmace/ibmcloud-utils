060cdaf8abece23d18e9caaa4dae477ec4051127
Lionel Mace <lionel.mace@gmail.com>
2023-12-16 11:04:30 +0100

    updated provider

--- terraform/private-roks-vpn/provider.tf
+++ terraform/private-roks-vpn/provider.tf
@@ -3,7 +3,7 @@
 ##############################################################################
 
 terraform {
-  required_version = ">=1.5, < 2.0.0"
+  required_version = ">=1.5, < 1.6"
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
