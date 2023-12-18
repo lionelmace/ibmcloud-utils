2ffce52765bf3881225ca3f5efc5944bcfe16ff3
Lionel Mace <lionel.mace@gmail.com>
2023-12-18 15:29:03 +0100

    added bastion

---
+++ terraform/private-roks-vpn/compute-vsi-mirror.tf
@@ -0,0 +1,80 @@
+
+variable "ssh_public_key" {
+  description = "SSH Public Key. Get your ssh key by running `ssh-key-gen` command"
+  type        = string
+  default     = null
+}
+
+variable "ssh_key_id" {
+  description = "ID of SSH public key stored in IBM Cloud"
+  type        = string
+  default     = null
+}
+
+variable "create_public_ip" {
+  type    = bool
+  default = true
+}
+
+variable "image_name" {
+  type        = string
+  default     = "ibm-ubuntu-20-04-6-minimal-amd64-3"
+  description = "Name of the image to use for the private instance"
+}
+
+variable "profile_name" {
+  type        = string
+  description = "Instance profile to use for the private instance"
+  default     = "cx2-2x4"
+}
+
+data "ibm_is_image" "image" {
+  name = var.image_name
+}
+
+resource "tls_private_key" "rsa_4096_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.rsa_4096_key.private_key_pem
+  filename        = "rsakey.pem"
+  file_permission = "0600"
+}
+
+# Generate an SSH Key
+resource "ibm_is_ssh_key" "generated_ssh_key" {
+  name           = "${local.basename}-ssh-key"
+  resource_group = ibm_resource_group.rg.id
+  public_key     = tls_private_key.rsa_4096_key.public_key_openssh
+}
+
+# Create a VSI (Virtual Server Instance)
+resource "ibm_is_instance" "vsi" {
+  name           = "${local.basename}-vsi"
+  vpc            = ibm_is_vpc.vpc.id
+  zone           = ibm_is_subnet.subnet-mirror.zone
+  profile        = var.profile_name
+  image          = data.ibm_is_image.image.id
+  keys           = [ibm_is_ssh_key.generated_ssh_key.id]
+  resource_group = ibm_resource_group.rg.id
+  tags           = var.tags
+
+  primary_network_interface {
+    subnet = ibm_is_subnet.subnet-mirror.id
+  }
+
+  boot_volume {
+    name = "${local.basename}-boot"
+  }
+}
+
+# A Public Floating IP for the VSI
+resource "ibm_is_floating_ip" "public_ip" {
+  count = tobool(var.create_public_ip) ? 1 : 0
+
+  name   = "${local.basename}-floating-ip"
+  target = ibm_is_instance.vsi.primary_network_interface[0].id
+  tags   = var.tags
+}
