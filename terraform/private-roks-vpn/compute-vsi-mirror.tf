80eaa7069088ad27acc7aa32f4a2eacf025e71d4
Lionel Mace <lionel.mace@gmail.com>
2023-12-18 15:34:48 +0100

    outputs

--- terraform/private-roks-vpn/compute-vsi-mirror.tf
+++ terraform/private-roks-vpn/compute-vsi-mirror.tf
@@ -78,3 +78,17 @@ resource "ibm_is_floating_ip" "public_ip" {
   target = ibm_is_instance.vsi.primary_network_interface[0].id
   tags   = var.tags
 }
+
+# Outputs
+output "vsi_private_ip" {
+  value = ibm_is_instance.vsi.primary_network_interface.0.primary_ip.0.address
+}
+
+output "vsi_public_ip" {
+  description = "The public Floating IP attached to the VSI"
+  value = ibm_is_floating_ip.public_ip.0.address
+}
+
+output "ssh_connection_string" {
+  value = "ssh -i ./rsakey.pem root@VSI-FLOATING-IP"
+}
