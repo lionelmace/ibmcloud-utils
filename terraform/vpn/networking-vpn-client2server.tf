dc17d4bac3b5dfd045ee2bbbcd3ba1c88bdcc8c7
Lionel Mace <lionel.mace@gmail.com>
2024-01-08 13:19:10 +0100

    added depends-on authorization for route

--- terraform/vpn/networking-vpn-client2server.tf
+++ terraform/vpn/networking-vpn-client2server.tf
@@ -84,9 +84,10 @@ resource "ibm_is_vpn_server_route" "route_nating" {
   action      = "translate"
   destination = "10.243.0.0/16"
   name        = "nating-frankfurt"
-  timeouts {
-    delete = "30m"
-  }
+  depends_on  = [ibm_iam_authorization_policy.secret_group_to_vpn]
+  # timeouts {
+  #   delete = "30m"
+  # }
 }
 
 resource "ibm_is_vpn_server_route" "route_cse_to_vpc" {
@@ -95,9 +96,10 @@ resource "ibm_is_vpn_server_route" "route_cse_to_vpc" {
   # destination = "166.9.0.0/16"
   destination = "166.8.0.0/14"
   name        = "route-2-ibm-cloud-service-endpoints"
-  timeouts {
-    delete = "30m"
-  }
+  depends_on  = [ibm_iam_authorization_policy.secret_group_to_vpn]
+  # timeouts {
+  #   delete = "30m"
+  # }
 }
 
 # allow clients to reach private backend
@@ -112,9 +114,10 @@ resource "ibm_is_vpn_server_route" "route_private_to_vpc" {
   action      = "deliver"
   destination = "161.26.0.0/16"
   name        = "route-private-2-ibm-iaas-endpoints"
-  timeouts {
-    delete = "30m"
-  }
+  depends_on  = [ibm_iam_authorization_policy.secret_group_to_vpn]
+  # timeouts {
+  #   delete = "30m"
+  # }
 }
 
 data "ibm_is_vpn_server_client_configuration" "config" {
