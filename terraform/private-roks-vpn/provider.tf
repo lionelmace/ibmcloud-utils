a12c3437f97dd61c1978bdc8c46ce2191e6f296f
Lionel Mace <lionel.mace@gmail.com>
2024-05-17 10:07:21 +0200

    upgraded tf cloud provider to 1.65.1

--- terraform/private-roks-vpn/provider.tf
+++ terraform/private-roks-vpn/provider.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.65.0"
+      version = "1.65.1"
     }
     logdna = {
       source  = "logdna/logdna"
