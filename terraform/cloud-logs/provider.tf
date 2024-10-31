9bd8b9cb4f6911598e0ee4281748baf94743a91d
Lionel Mace <lionel.mace@gmail.com>
2024-10-31 10:16:32 +0100

    updated tf for cloud logs

--- terraform/cloud-logs/provider.tf
+++ terraform/cloud-logs/provider.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     ibm = {
       source  = "IBM-Cloud/ibm"
-      version = "1.69.2"
+      version = "1.70.1"
     }
     # logdna = {
     #   source  = "logdna/logdna"
